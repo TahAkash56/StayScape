adbdcf56e678a598d0d66e83f92623747dc9a14c
Akash Tah <tahakash56@gmail.com>
2026-01-07 00:49:46 +0530

    Phase-1 Part b

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/StayScape";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+async function initDB() {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("database was initialized");
+}
+
+
+main()
+    .then(() => {
+        console.log("connected to database");
+        return initDB();
+    })
+    .catch(err => {
+        console.log(err);
+    });
